2c78270552d71c9c2119682a80e84de080b51924
Gokul Sundeep <150696266+GokulSundeep29@users.noreply.github.com>
2025-08-16 04:50:37 +0530

    add utils.py

--- src/mcqgen/mcq_generator.py
+++ src/mcqgen/mcq_generator.py
@@ -34,4 +34,38 @@ quiz_generation_prompt = PromptTemplate(
     template=PROMPT
 )
 
+# LLM Model
+llm = ChatOpenAI(model=model_name, temperature=1)
+
+# Web Search Tool
+search = TavilySearchResults(tavily_api_key=os.getenv("TAVILY_API_KEY"), max_results=3)
+
+topics_list = [
+    {"topic": "AI Agents", "difficulty": "Medium, Hard", "count": 4, "recent": True},
+    {"topic": "English Grammar", "difficulty": "Easy", "count": 3, "recent": False},
+    {"topic": "Cloud Computing", "difficulty": "Easy, Medium", "count": 5, "recent": True}
+]
+
+# Build topics string
+topics_str = "\n".join([f"{t['topic']} - {t['difficulty']} - generate {t['count']} questions" for t in topics_list])
+
+## Collect context
+context_parts = []
+for t in topics_list:
+    if t.get("recent"):
+        search_results = search.run(t["topic"])
+        context_parts.append(f"Topic: {t['topic']}\nSearch Results:\n{search_results}")
+context_str = "\n\n".join(context_parts) if context_parts else "No external context required."
+
+# Format final prompt
+final_prompt = quiz_generation_prompt.format(
+    topics_difficulty_counts=topics_str,
+    context=context_str,
+    response_json = json.dumps(RESPONSE_JSON)
+)
+
+with get_openai_callback() as cb:
+    response = llm.invoke(final_prompt)
     
+result_dict = json.loads(response.content)
+print(result_dict)
---
+++ src/mcqgen/utils.py
@@ -0,0 +1,7 @@
+import json
+
+def convert_to_list_of_dict(data):
+    pass
+
+def download_as_word(response):
+    pass
